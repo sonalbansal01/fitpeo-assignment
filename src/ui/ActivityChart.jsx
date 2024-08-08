7e05bd0724f45fc0d505826c67f4216df469bc56
sonalbansal <sonal@jb.com>
2024-08-08 16:54:43 +0530

    chart-update

--- src/ui/ActivityChart.jsx
+++ src/ui/ActivityChart.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
+import { Box, Slider } from "@mui/material";
 import {
-  Box,
-  Typography,
-  Slider,
-  FormControlLabel,
-  Checkbox,
-} from "@mui/material";
-import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 // Custom bar shape to add border-radius
 const CustomBar = (props) => {
@@ -17,11 +18,10 @@ const CustomBar = (props) => {
       <rect
         x={x}
         y={y}
-        width={30}
+        width={width}
         height={height}
         fill={fill}
-        radius={90} // Adjust radius as needed
-        style={{ borderRadius: "6rem" }} // For border-radius
+        rx={25} // Border radius of the rectangle
       />
     </g>
   );
@@ -30,71 +30,39 @@ const CustomBar = (props) => {
 const series = [
   {
     label: "series 1",
-    data: [
-      223, 2210, 764, 1879, 1478, 1373, 1891, 2171, 620, 1269, 724, 1707, 1188,
-      1879, 626, 1635, 2177, 516, 1793, 1598,
-    ],
+    data: [923, 2210, 1764, 1879, 1478, 1373, 791, 1271, 620, 829],
   },
 ];
 
 const ActivityChart = () => {
   const [itemNb, setItemNb] = useState(10);
-  const [seriesNb, setSeriesNb] = useState(1);
-  const [skipAnimation, setSkipAnimation] = useState(false);
 
   const handleItemNbChange = (event, newValue) => setItemNb(newValue);
-  const handleSeriesNbChange = (event, newValue) => setSeriesNb(newValue);
 
   return (
     <div className="p-4 bg-white dark:bg-gray-800 w-full rounded-lg shadow-md col-span-2">
       <h2 className="text-lg font-bold mb-4">Activity</h2>
       <Box>
-        <BarChart
-          width={500}
-          height={250}
-          data={series[0].data.slice(0, itemNb).map((value, index) => ({
-            name: `Item ${index + 1}`,
-            value,
-          }))}
-          margin={{ top: 10, right: 30, left: 20, bottom: 5 }}
-        >
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey="value" fill="#8884d8" shape={<CustomBar />} />
-        </BarChart>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={skipAnimation}
-              onChange={(event) => setSkipAnimation(event.target.checked)}
+        <ResponsiveContainer width="90%" height={210}>
+          <BarChart
+            data={series[0].data.slice(0, itemNb).map((value, index) => ({
+              name: ` ${index + 1}`,
+              value,
+            }))}
+            margin={{ top: 10, right: 0, left: 20, bottom: 5 }}
+          >
+            <XAxis dataKey="name" />
+            <YAxis minTickGap={15} domain={[10, "dataMax"]} />{" "}
+            {/* Set min to a value greater than 0 */}
+            <Tooltip />
+            <Bar
+              dataKey="value"
+              fill="#3ca3fd"
+              shape={<CustomBar />}
+              barSize={30}
             />
-          }
-          label="Skip Animation"
-          labelPlacement="end"
-        />
-        <Typography id="input-item-number" gutterBottom>
-          Number of items
-        </Typography>
-        <Slider
-          value={itemNb}
-          onChange={handleItemNbChange}
-          valueLabelDisplay="auto"
-          min={1}
-          max={20}
-          aria-labelledby="input-item-number"
-        />
-        <Typography id="input-series-number" gutterBottom>
-          Number of series
-        </Typography>
-        <Slider
-          value={seriesNb}
-          onChange={handleSeriesNbChange}
-          valueLabelDisplay="auto"
-          min={1}
-          max={10}
-          aria-labelledby="input-series-number"
-        />
+          </BarChart>
+        </ResponsiveContainer>
       </Box>
     </div>
   );
